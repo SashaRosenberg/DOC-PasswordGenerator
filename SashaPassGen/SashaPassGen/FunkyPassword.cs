99d0f61e6b52276ba89d3b258d2e2d5cc3d76418
SashaRosenberg <54061573+SashaRosenberg@users.noreply.github.com>
2019-08-19 11:10:41 +0800

    First live build
    this build generates a password using a random list of adjectives, animal names, and two numbers (0-9)

---
+++ SashaPassGen/SashaPassGen/FunkyPassword.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+/// <summary>
+/// this code was designed and written by Sasha Rosenberg
+/// feel free to take it, but plz credit <3
+/// also if you're looking at this to hire me, this was done in three hours from concept to delivery inbetween calls
+/// Version 1.0
+/// todo: add crash prevention and text clensing
+/// </summary>
+
+namespace SashaPassGen
+{
+
+
+    public class FunkyPassword : Form
+    {
+        //public List<string> Animals;
+        // public List<>
+
+        static string password;
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new GUI());
+            GenerateStuff GS = new GenerateStuff();
+            GS.GeneratePassword();
+            password = GS.FinalOutput;
+            //get list of adjectives
+            Console.WriteLine(password + "!!!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            GS.GeneratePassword();
+            password = GS.FinalOutput;
+            //get list of adjectives
+            Console.WriteLine(password + "!!!");
+        }
+
+    }
+    public class GenerateStuff
+    {
+        public string FinalOutput;
+        public Random rnd1 = new Random();
+        public Random rnd2 = new Random();
+        public Random rnd3 = new Random();
+        int Adjectives, Noun, Number = 0;
+        int Num_store = 99;
+        public string[] linesAD, linesNoun;
+
+
+        void checkRandomShit()
+        {
+            linesAD = Properties.Resources.adjectives.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            linesNoun = Properties.Resources.animals.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            Adjectives = rnd1.Next(linesAD.Length);
+            Noun = rnd2.Next(linesNoun.Length);
+            while (Noun == Adjectives)
+            {
+                Noun = rnd2.Next(linesNoun.Length);
+            }
+
+            Number = rnd3.Next(10, Num_store);
+            while (Number == Adjectives || Number == Noun)
+            {
+                Number = rnd3.Next(10, Num_store);
+            }
+            Console.WriteLine(Adjectives + Noun + Number);
+
+        }
+        public void GeneratePassword()
+        {
+            checkRandomShit();
+            FinalOutput = genAdjectiveList() + genNounList() + GenNumber();
+        }
+
+        string genAdjectiveList()
+        {
+            string Result = "Blank";
+
+            Result = linesAD[Adjectives];
+            Result =  char.ToUpper(Result[0]) + Result.Substring(1).ToLower();
+
+            return Result;
+        }
+        string GenNumber()
+        {
+            return Number.ToString();
+        }
+        string genNounList()
+        {
+            string Result = "Blank";
+            Result = linesNoun[Noun];
+            return char.ToUpper(Result[0]) + Result.Substring(1).ToLower();
+        }
+
+    }
+
+}
+
---
+++ SashaPassGen/SashaPassGen/GUI.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SashaPassGen
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Copy = new System.Windows.Forms.Button();
+            this.Generate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(191, 100);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(75, 23);
+            this.Copy.TabIndex = 4;
+            this.Copy.Text = "Copy";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click_1);
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(12, 100);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(173, 23);
+            this.Generate.TabIndex = 3;
+            this.Generate.Text = "Generate";
+            this.Generate.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = global::SashaPassGen.Properties.Resources.communities_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(337, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(334, 128);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.Generate);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GUI";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "DOC Password Generator";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.GUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
